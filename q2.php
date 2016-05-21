823c8cfca57c3eeb92106f6caa28db6716e55969
Bruce Ediger <bediger@stratigery.com>
2016-05-20 20:01:16 -0600

    Self-replicating programs, Python, PHP, JavaScript/HTML, and an ourobours/quine-relay in Perl, Python, PHP and bash.

---
+++ q2.php
@@ -0,0 +1,4 @@
+<?php
+$genome='echo chr(60).chr(63).chr(112).chr(104).chr(112).chr(10); echo chr(36)."genome=".chr(39).$genome.chr(39).chr(59).chr(10); echo $genome.chr(10).chr(63).chr(62).chr(10);';
+echo chr(60).chr(63).chr(112).chr(104).chr(112).chr(10); echo chr(36)."genome=".chr(39).$genome.chr(39).chr(59).chr(10); echo $genome.chr(10).chr(63).chr(62).chr(10);
+?>
---
+++ self_replicating.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+S="'"; D='"'; E=';'; C=','; B='#!/usr/bin/env python'
+G='print B; print "S="+D+S+D+E, "D="+S+D+S+E, "E="+S+E+S+E, "C="+S+C+S+E, "B="+S+B+S; print "G="+S+G+S; print G'
+print B; print "S="+D+S+D+E, "D="+S+D+S+E, "E="+S+E+S+E, "C="+S+C+S+E, "B="+S+B+S; print "G="+S+G+S; print G
